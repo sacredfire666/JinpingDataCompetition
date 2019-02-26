2009b02ca28346babfd34bc20b5ac27416f49eea
guozy <guozy1992@gmail.com>
2019-02-26 21:48:32 +0800

    Update file structure

--- 1tPrototype/ConvertTruth.py
+++ 1tPrototype/ConvertTruth.py
@@ -18,9 +18,8 @@ FileNo = 0
 windowSize = 1029
 
 f = h5py.File(outputFileName, "w")
-g1 = f.create_group("TriggerInfo")
-g2 = f.create_group("Waveform")
-g3 = f.create_group("GroundTruth")
+g1 = f.create_group("Waveform")
+g2 = f.create_group("GroundTruth")
 
 # Loop for ROOT files
 while True:
@@ -38,39 +37,35 @@ while True:
 
     # Allocate memery first, for efficiency
     nTrigger = t.GetEntries()
-    evtid = np.zeros((nTrigger,), dtype="int64")
-    sec = np.zeros((nTrigger,), dtype="int64")
-    nanosec = np.zeros((nTrigger,), dtype="int64")
     ch_entry = t.Draw("ChannelId","","goff")
     ch_evtid = np.zeros((ch_entry,), dtype="int64")
-    chlid = np.zeros((ch_entry,), dtype='int16')
-    wav = np.zeros((ch_entry*windowSize,), dtype='int16')
+    ch_sec   = np.zeros((ch_entry,), dtype='int64')
+    ch_nsec  = np.zeros((ch_entry,), dtype='int64')
+    ch_id    = np.zeros((ch_entry,), dtype='int16')
+    ch_wav   = np.zeros((ch_entry*windowSize,), dtype='int16')
 
 
     # Loop for event, fill the arrays
     ch_idx = 0
     wav_idx = 0
-    trig_idx = 0
+    #trig_idx = 0
     for event in t:
         nChannel = event.ChannelId.size()
         wavSize = event.Waveform.size()
 
-        evtid[trig_idx:trig_idx+1] = event.TriggerNo
-        sec[trig_idx:trig_idx+1] = event.Sec
-        nanosec[trig_idx:trig_idx+1] = event.NanoSec
-        trig_idx = trig_idx+1
-
         ch_evtid[ch_idx:ch_idx+nChannel] = event.TriggerNo
-        chlid[ch_idx:ch_idx+nChannel] = list(event.ChannelId)
+        ch_sec[ch_idx:ch_idx+nChannel] = event.Sec
+        ch_nsec[ch_idx:ch_idx+nChannel] = event.NanoSec
+        ch_id[ch_idx:ch_idx+nChannel] = list(event.ChannelId)
         ch_idx = ch_idx+nChannel
-        wav[wav_idx:wav_idx+wavSize] = list(event.Waveform)
+        ch_wav[wav_idx:wav_idx+wavSize] = list(event.Waveform)
         wav_idx = wav_idx+wavSize
     
     nTriggerTruth = tTruth.GetEntries()
     pe_entry_truth = tTruth.Draw("PEList.PMTId","","goff")
     pe_evtid_truth = np.zeros((pe_entry_truth,), dtype="int64")
     pe_pmtid_truth = np.zeros((pe_entry_truth,), dtype="int64")
-    pe_time_truth = np.zeros((pe_entry_truth,), dtype="int64")
+    pe_time_truth  = np.zeros((pe_entry_truth,), dtype="int64")
     pe_idx = 0
     for event in tTruth:
         trigger_id = event.TriggerNo
@@ -81,41 +76,37 @@ while True:
             pe_idx = pe_idx+1
 
     # Create dataset
-    wav = wav.reshape(-1,windowSize)
-    if FileNo==0:
-        dset0 = g1.create_dataset("EventID", data=evtid, maxshape=(None,))
-        dset1 = g1.create_dataset("Sec",     data=sec, maxshape=(None,))
-        dset2 = g1.create_dataset("NanoSec", data=nanosec, maxshape=(None,))
+    ch_wav = ch_wav.reshape(-1,windowSize)
+    if FileNo==0:   # Create new data set
 
-        dset3 = g2.create_dataset("EventID", data=ch_evtid, maxshape=(None,))
-        dset4 = g2.create_dataset("ChannelID", data = chlid, maxshape=(None,))
-        dset5 = g2.create_dataset("Waveform", data = wav, maxshape=(None,windowSize), compression="gzip", compression_opts=4)
+        dset0 = g1.create_dataset("EventID", data=ch_evtid, maxshape=(None,))
+        dset1 = g1.create_dataset("ChannelID", data = ch_id, maxshape=(None,))
+        dset2 = g1.create_dataset("Sec", data = ch_sec, maxshape=(None,))
+        dset3 = g1.create_dataset("NanoSec", data = ch_nsec, maxshape=(None,))
+        dset4 = g1.create_dataset("Waveform", data = ch_wav, maxshape=(None,windowSize), compression="gzip", compression_opts=4)
 
-        dset6 = g3.create_dataset("EventID", data=pe_evtid_truth, maxshape=(None,))
-        dset7 = g3.create_dataset("ChannelID", data=pe_pmtid_truth, maxshape=(None,))
-        dset8 = g3.create_dataset("PETime", data=pe_time_truth, maxshape=(None,))
+        dset5 = g2.create_dataset("EventID", data=pe_evtid_truth, maxshape=(None,))
+        dset6 = g2.create_dataset("ChannelID", data=pe_pmtid_truth, maxshape=(None,))
+        dset7 = g2.create_dataset("PETime", data=pe_time_truth, maxshape=(None,))
 
-    else:
-        dset0.resize(dset0.shape[0]+nTrigger, axis=0)
-        dset1.resize(dset1.shape[0]+nTrigger, axis=0)
-        dset2.resize(dset2.shape[0]+nTrigger, axis=0)
-        dset0[-evtid.shape[0]:] = evtid
-        dset1[-sec.shape[0]:] = sec
-        dset2[-nanosec.shape[0]:] = nanosec
-        dset3.resize(dset3.shape[0]+ch_entry, axis=0)
+    else:   # Insert new data to the data set
         
-        dset4.resize(dset4.shape[0]+ch_entry, axis=0)
-        dset5.resize(dset5.shape[0]+ch_entry, axis=0)
-        dset3[-ch_evtid.shape[0]:] = ch_evtid
-        dset4[-chlid.shape[0]:] = chlid
-        dset5[-wav.shape[0]:] = wav
+        dset0.resize(dset0.shape[0]+ch_entry, axis=0)
+        dset1.resize(dset1.shape[0]+ch_entry, axis=0)
+        dset2.resize(dset2.shape[0]+ch_entry, axis=0)
+        dset3.resize(dset3.shape[0]+ch_entry, axis=0)
+        dset0[-ch_evtid.shape[0]:] = ch_evtid
+        dset1[-ch_id.shape[0]:] = ch_id
+        dset2[-ch_sec.shape[0]:] = ch_sec
+        dset3[-ch_nsec.shape[0]:] = ch_nsec
+        dset4[-ch_wav.shape[0]:] = ch_wav
 
-        dset6.resize(dset6.shape[0]+nTriggerTruth, axis=0)
-        dset7.resize(dset7.shape[0]+nTriggerTruth, axis=0)
-        dset8.resize(dset8.shape[0]+nTriggerTruth, axis=0)
-        dset6[-pe_evtid_truth.shape[0]:] = pe_evtid_truth
-        dset7[-pe_pmtid_truth.shape[0]:] = pe_pmtid_truth
-        dset8[-pe_time_truth.shape[0]:] = pe_time_truth
+        dset5.resize(dset6.shape[0]+nTriggerTruth, axis=0)
+        dset6.resize(dset7.shape[0]+nTriggerTruth, axis=0)
+        dset7.resize(dset8.shape[0]+nTriggerTruth, axis=0)
+        dset5[-pe_evtid_truth.shape[0]:] = pe_evtid_truth
+        dset6[-pe_pmtid_truth.shape[0]:] = pe_pmtid_truth
+        dset7[-pe_time_truth.shape[0]:] = pe_time_truth
     
     FileNo = FileNo+1
 
--- 1tPrototype/ReadExample.py
+++ 1tPrototype/ReadExample.py
@@ -6,12 +6,16 @@ matplotlib.use("agg")
 import matplotlib.pyplot as plt
 
 # Read hdf5 file
-filename = "data/Run669.h5"
+filename = "test.h5"
 f = h5py.File(filename,"r")
 evtid = f["Waveform/EventID"][:]
 chlid = f["Waveform/ChannelID"][:]
 wav = f["Waveform/Waveform"][:]
 wav = np.column_stack((chlid,wav))
+
+evtid_truth = f["GroundTruth/EventID"][:]
+chlid_truth = f["GroundTruth/ChannelID"][:]
+PETime = f["GroundTruth/PETime"][:]
 
 column_name = ["ChannelID"]
 for i in range(1029):
@@ -24,17 +28,22 @@ wavdf = pd.DataFrame(wav, columns=column_name, index=evtid)
 eid = 1
 print(wavdf.loc[eid]["ChannelID"])
 
-# Plot the waveform of #2 PMT
-wav_e1p2 = wavdf.loc[eid].query("ChannelID==2")
+# Plot the waveform of #0 PMT
+wav_e1p2 = wavdf.loc[eid].query("ChannelID==0")
 wav_e1p2 = wav_e1p2.iloc[0].values[1:]
 plt.plot(wav_e1p2)
 plt.xlabel('Time [ns]')
 plt.ylabel('Voltage [ADC]')
+
+time1 = PETime[0]
+plt.plot([time1,time1],[940,980])
+
 plt.savefig("example.png")
+
 
 # Calculate the pedestal
 ped = np.mean(wavdf.iloc[:,1:150], axis=1)
 peddf = pd.DataFrame({'ChannelID': chlid, 'Pedestal': ped})
-print(peddf)
+#print(peddf)
 
 f.close()
